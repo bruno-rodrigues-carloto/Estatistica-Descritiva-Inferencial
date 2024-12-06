761092a512e15c26ce3eb2f244db27baf3299b55
Bruno Rodrigues Carloto <brunnocarlotosjob@gmail.com>
2024-12-05 23:26:17 -0300

    Update calculadora_ic_inadimplencia.py

--- IC_Inadimplencia/calculadora_ic_inadimplencia.py
+++ IC_Inadimplencia/calculadora_ic_inadimplencia.py
@@ -27,60 +27,57 @@ st.sidebar.markdown("- [Mercadados](https://brunnocarlotosjob.wixsite.com/mercad
 tamanho_amostra = st.number_input("Insira o volume total da amostra: ", min_value=0.0, max_value=1.0, step=0.001)
 tamanho_amostra_convertido = st.number_input("Insira o volume total da amostra convertida no Positivo: ", min_value=0.0, max_value=1.0, step=0.001)
 inad_positivo = st.number_input("Insira a inadimplência Positivo: ", min_value=0.0, max_value=1.0, step=0.001)
-
-# Calculando erro padrão para margem de erro
-try:
-    erro_padrao = np.sqrt( ((inad_positivo * (1 - inad_positivo))/tamanho_amostra_convertido) )
-    
-except ZeroDivisionError:
-    st.write("Denominador não pode ser zero.")
-
-# Análise de cumprimento das premissas de volumetria
-
-if (tamanho_amostra_convertido * inad_positivo >= 10) and (tamanho_amostra_convertido * (1 - inad_positivo) >= 10):
-    print("Os números de inadimplentes e adimplentes são suficientes.")
-    
-elif (tamanho_amostra_convertido * inad_positivo >= 10) and (tamanho_amostra_convertido * (1 - inad_positivo) < 10):
-    print("Os números de inadimplentes é suficiente, mas o número de adimplentes não é.")
-    
-elif (tamanho_amostra_convertido * inad_positivo < 10) and (tamanho_amostra_convertido * (1 - inad_positivo) >= 10):
-    print("O número de adimplentes é suficiente, mas o número de inadimplentes não é.")
-    
-elif (tamanho_amostra_convertido * inad_positivo < 10) and (tamanho_amostra_convertido * (1 - inad_positivo) < 10):
-    print("Os números de inadimplentes e adimplentes NÃO são suficientes.")
-
-
-# Definições dos Z's
-Z_nivel_90 = 1.645
-Z_nivel_95 = 1.96
-Z_nivel_99 = 2.576
-
-# Selecionar nível de confiança
 confianca = st.sidebar.selectbox('Selecione a página', ['99%', '95%', '90%'])
 
-# Buscando Zcrítico
-if confianca == '99%':
-  ic_inferior = round(inad_positivo - Z_nivel_99 * erro_padrao, 4) * 100
-  ic_superior = round(inad_positivo + Z_nivel_99 * erro_padrao, 4) * 100
-  
-  st.write(f"Com 99% de confiança, a indimplência da população está entre {ic_inferior}% e {ic_superior}.")
+# Calculando erro padrão para margem de erro
+if tamanho_amostra_convertido == 0:
+    st.write("")
+    erro_padrao = np.sqrt( ((inad_positivo * (1 - inad_positivo))/tamanho_amostra_convertido) )
+    
+else:
+    # Análise de cumprimento das premissas de volumetria
+    if (tamanho_amostra_convertido * inad_positivo >= 10) and (tamanho_amostra_convertido * (1 - inad_positivo) >= 10):
+        print("Os números de inadimplentes e adimplentes são suficientes.")
+        
+    elif (tamanho_amostra_convertido * inad_positivo >= 10) and (tamanho_amostra_convertido * (1 - inad_positivo) < 10):
+        print("Os números de inadimplentes é suficiente, mas o número de adimplentes não é.")
+        
+    elif (tamanho_amostra_convertido * inad_positivo < 10) and (tamanho_amostra_convertido * (1 - inad_positivo) >= 10):
+        print("O número de adimplentes é suficiente, mas o número de inadimplentes não é.")
+        
+    elif (tamanho_amostra_convertido * inad_positivo < 10) and (tamanho_amostra_convertido * (1 - inad_positivo) < 10):
+        print("Os números de inadimplentes e adimplentes NÃO são suficientes.")
 
 
-elif confianca == '95%':
-  ic_inferior = round(inad_positivo - Z_nivel_95 * erro_padrao, 4) * 100
-  ic_superior = round(inad_positivo + Z_nivel_95 * erro_padrao, 4) * 100
-  
-  st.write(f"Com 95% de confiança, a indimplência da população está entre {ic_inferior}% e {ic_superior}.")
-
-elif confianca == '90%':
-  ic_inferior = round(inad_positivo - Z_nivel_90 * erro_padrao, 4) * 100
-  ic_superior = round(inad_positivo + Z_nivel_90 * erro_padrao, 4) * 100
-  
-  st.write(f"Com 90% de confiança, a indimplência da população está entre {ic_inferior}% e {ic_superior}.")
-
-           
-
-# parcela = st.radio('Selecione a quantidade de parcela',(1, 2, 3, 4, 5))
-# st.write('Valor à vista se o empréstimo for aprovado: R$ {} por mês'.format(mensalidade))
-# st.button("Ver Resultado")
-# usuario =  st.text_input('Me informe seu nome para termos uma interação melhor.')
+    # Definições dos Z's
+    Z_nivel_90 = 1.645
+    Z_nivel_95 = 1.96
+    Z_nivel_99 = 2.576
+    
+    
+    # Buscando Zcrítico
+    if confianca == '99%':
+      ic_inferior = round(inad_positivo - Z_nivel_99 * erro_padrao, 4) * 100
+      ic_superior = round(inad_positivo + Z_nivel_99 * erro_padrao, 4) * 100
+      
+      st.write(f"Com 99% de confiança, a indimplência da população está entre {ic_inferior}% e {ic_superior}.")
+    
+    
+    elif confianca == '95%':
+      ic_inferior = round(inad_positivo - Z_nivel_95 * erro_padrao, 4) * 100
+      ic_superior = round(inad_positivo + Z_nivel_95 * erro_padrao, 4) * 100
+      
+      st.write(f"Com 95% de confiança, a indimplência da população está entre {ic_inferior}% e {ic_superior}.")
+    
+    elif confianca == '90%':
+      ic_inferior = round(inad_positivo - Z_nivel_90 * erro_padrao, 4) * 100
+      ic_superior = round(inad_positivo + Z_nivel_90 * erro_padrao, 4) * 100
+      
+      st.write(f"Com 90% de confiança, a indimplência da população está entre {ic_inferior}% e {ic_superior}.")
+    
+               
+    
+    # parcela = st.radio('Selecione a quantidade de parcela',(1, 2, 3, 4, 5))
+    # st.write('Valor à vista se o empréstimo for aprovado: R$ {} por mês'.format(mensalidade))
+    # st.button("Ver Resultado")
+    # usuario =  st.text_input('Me informe seu nome para termos uma interação melhor.')
