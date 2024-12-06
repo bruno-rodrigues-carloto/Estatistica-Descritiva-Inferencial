4eba2e4c10c7380598817ead80b14638b772cd1f
Bruno Rodrigues Carloto <brunnocarlotosjob@gmail.com>
2024-12-05 23:54:50 -0300

    Update calculadora_ic_inadimplencia.py

--- IC_Inadimplencia/calculadora_ic_inadimplencia.py
+++ IC_Inadimplencia/calculadora_ic_inadimplencia.py
@@ -36,7 +36,9 @@ if tamanho_amostra_convertido == 0:
 else:
     st.write(f'''
     __Volume amostral:__ {tamanho_amostra}
+    
     __Volume amostral convertido:__ {tamanho_amostra_convertido}
+    
     __Inadimplência amostral convertido:__ {inad_positivo * 100}%
     
     ''')
