6ed66eed4ef5fd9ce6f37de3792a39e572aad315
Bruno Rodrigues Carloto <brunnocarlotosjob@gmail.com>
2024-12-07 00:03:23 -0300

    Update calculadora_ic_inadimplencia.py

--- IC_Inadimplencia/calculadora_ic_inadimplencia.py
+++ IC_Inadimplencia/calculadora_ic_inadimplencia.py
@@ -4,14 +4,14 @@ from PIL import Image
 import streamlit as st
 
 #Indicando sidebar
-st.markdown("<h1 style='font-size: 24px; color: black; font-weight: bold;'>Calculadora de Intervalo de Confiança de inadimplentes</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='font-size: 24px; color: black; font-weight: bold;'>Calculadora de Intervalo de Confiança de Proporção</h1>", unsafe_allow_html=True)
 
 #Indicando do que se trata a web app
 foto = Image.open('IC_Inadimplencia/bruno_carloto.jpg')
 st.sidebar.image(foto, use_column_width=True, width=50)
 st.sidebar.subheader('Bruno Rodrigues Carloto')
 st.sidebar.markdown('Analista de dados e Política de Crédito')
-st.sidebar.markdown('#### Projeto de Estatística Inferencial')
+st.sidebar.markdown('#### Projeto de Estatística Inferencial - IC para proporção')
 
 st.sidebar.markdown("Redes Sociais :")
 st.sidebar.markdown("- [Linkedin](https://www.linkedin.com/in/bruno-rodrigues-carloto)")
@@ -19,12 +19,12 @@ st.sidebar.markdown("- [Medium](https://medium.com/@brc-deep-analytics)")
 st.sidebar.markdown("- [Mercadados](https://brunnocarlotosjob.wixsite.com/mercadados)")
 
 tamanho_populacao = st.number_input("Insira o volume total da população - os quais nem todos são performados -, caso haja: ", min_value=0)
-tamanho_amostra_convertido = st.number_input("Insira o volume total da amostra performada: ", min_value=0)
-inad_positivo = st.number_input("Insira a inadimplência: ", min_value=0.0, max_value=1.0, step=0.0001)
+tamanho_amostra = st.number_input("Insira o volume total da amostra: ", min_value=0)
+proporcao = st.number_input("Insira a proporção em forma decimal: ", min_value=0.0, max_value=1.0, step=0.0001)
 confianca = st.selectbox('Selecione a página', ['99%', '95%', '90%'])
 
 # Calculando erro padrão para margem de erro
-if tamanho_amostra_convertido == 0:
+if tamanho_amostra == 0:
     st.write("")
     
 else:
@@ -32,13 +32,13 @@ else:
     st.write(f'''
     Volume população: {tamanho_populacao}
     
-    Volume amostral performado: {tamanho_amostra_convertido}
+    Volume amostra: {tamanho_amostra}
     
-    Inadimplência amostral: {inad_positivo * 100}%
+    Proporção amostral: {proporcao * 100}%
     
     ''')
     # Calculando erro padrão
-    erro_padrao = np.sqrt( ((inad_positivo * (1 - inad_positivo))/tamanho_amostra_convertido) )
+    erro_padrao = np.sqrt( ((proporcao * (1 - proporcao))/tamanho_amostra) )
     
     # Definições dos Z's
     Z_nivel_90 = 1.645
@@ -51,60 +51,60 @@ else:
         
         # Buscando Zcrítico
         if confianca == '99%':
-          ic_inferior = np.round(inad_positivo - Z_nivel_99 * erro_padrao, 4) * 100
-          ic_superior = np.round(inad_positivo + Z_nivel_99 * erro_padrao, 4) * 100
+          ic_inferior = np.round(proporcao - Z_nivel_99 * erro_padrao, 4) * 100
+          ic_superior = np.round(proporcao + Z_nivel_99 * erro_padrao, 4) * 100
           st.markdown("<h1 style='font-size: 16px; color: red; font-weight: bold;'>Resultado:</h1>", unsafe_allow_html=True)
-          st.write(f"Com 99% de confiança, a indimplência da população está entre {np.round(ic_inferior, 2)}% e {np.round(ic_superior, 2)}%.")
+          st.write(f"Com 99% de confiança, a proporção da população está entre {np.round(ic_inferior, 2)}% e {np.round(ic_superior, 2)}%.")
         
         elif confianca == '95%':
-          ic_inferior = round(inad_positivo - Z_nivel_95 * erro_padrao, 4) * 100
-          ic_superior = round(inad_positivo + Z_nivel_95 * erro_padrao, 4) * 100
+          ic_inferior = round(proporcao - Z_nivel_95 * erro_padrao, 4) * 100
+          ic_superior = round(proporcao + Z_nivel_95 * erro_padrao, 4) * 100
           st.markdown("<h1 style='font-size: 16px; color: red; font-weight: bold;'>Resultado:</h1>", unsafe_allow_html=True)
-          st.write(f"Com 95% de confiança, a indimplência da população está entre {np.round(ic_inferior, 2)}% e {np.round(ic_superior, 2)}%.")
+          st.write(f"Com 95% de confiança, a proporção da população está entre {np.round(ic_inferior, 2)}% e {np.round(ic_superior, 2)}%.")
         
         elif confianca == '90%':
-          ic_inferior = round(inad_positivo - Z_nivel_90 * erro_padrao, 4) * 100
-          ic_superior = round(inad_positivo + Z_nivel_90 * erro_padrao, 4) * 100
+          ic_inferior = round(proporcao - Z_nivel_90 * erro_padrao, 4) * 100
+          ic_superior = round(proporcao + Z_nivel_90 * erro_padrao, 4) * 100
           st.markdown("<h1 style='font-size: 16px; color: red; font-weight: bold;'>Resultado:</h1>", unsafe_allow_html=True)
-          st.write(f"Com 90% de confiança, a indimplência da população está entre {np.round(ic_inferior, 2)}% e {np.round(ic_superior, 2)}%.")
+          st.write(f"Com 90% de confiança, a proporção da população está entre {np.round(ic_inferior, 2)}% e {np.round(ic_superior, 2)}%.")
         
     else:
         # Análise de cumprimento das premissas de volumetria
-        if (tamanho_amostra_convertido * inad_positivo >= 10) and (tamanho_amostra_convertido * (1 - inad_positivo) >= 10):
+        if (tamanho_amostra * proporcao >= 5) and (tamanho_amostra * (1 - proporcao) >= 5):
             st.markdown("<h1 style='font-size: 16px; color: gray; font-weight: bold;'>Premissas de volume:</h1>", unsafe_allow_html=True)
-            st.write("Os números de inadimplentes e adimplentes são suficientes para o cálculo.")
+            st.write("Os números de sucesso e fracasso são suficientes para o cálculo.")
         
         
             # Buscando Zcrítico
             if confianca == '99%':
-              ic_inferior = np.round(inad_positivo - Z_nivel_99 * erro_padrao, 4) * 100
-              ic_superior = np.round(inad_positivo + Z_nivel_99 * erro_padrao, 4) * 100
+              ic_inferior = np.round(proporcao - Z_nivel_99 * erro_padrao, 4) * 100
+              ic_superior = np.round(proporcao + Z_nivel_99 * erro_padrao, 4) * 100
               st.markdown("<h1 style='font-size: 16px; color: red; font-weight: bold;'>Resultado:</h1>", unsafe_allow_html=True)
-              st.write(f"Com 99% de confiança, a indimplência da população está entre {np.round(ic_inferior, 2)}% e {np.round(ic_superior, 2)}%.")
+              st.write(f"Com 99% de confiança, a proporção da população está entre {np.round(ic_inferior, 2)}% e {np.round(ic_superior, 2)}%.")
             
             
             elif confianca == '95%':
-              ic_inferior = round(inad_positivo - Z_nivel_95 * erro_padrao, 4) * 100
-              ic_superior = round(inad_positivo + Z_nivel_95 * erro_padrao, 4) * 100
+              ic_inferior = round(proporcao - Z_nivel_95 * erro_padrao, 4) * 100
+              ic_superior = round(proporcao + Z_nivel_95 * erro_padrao, 4) * 100
               st.markdown("<h1 style='font-size: 16px; color: red; font-weight: bold;'>Resultado:</h1>", unsafe_allow_html=True)
-              st.write(f"Com 95% de confiança, a indimplência da população está entre {np.round(ic_inferior, 2)}% e {np.round(ic_superior, 2)}%.")
+              st.write(f"Com 95% de confiança, a proporção da população está entre {np.round(ic_inferior, 2)}% e {np.round(ic_superior, 2)}%.")
             
             elif confianca == '90%':
-              ic_inferior = round(inad_positivo - Z_nivel_90 * erro_padrao, 4) * 100
-              ic_superior = round(inad_positivo + Z_nivel_90 * erro_padrao, 4) * 100
+              ic_inferior = round(proporcao - Z_nivel_90 * erro_padrao, 4) * 100
+              ic_superior = round(proporcao + Z_nivel_90 * erro_padrao, 4) * 100
               st.markdown("<h1 style='font-size: 16px; color: red; font-weight: bold;'>Resultado:</h1>", unsafe_allow_html=True)
-              st.write(f"Com 90% de confiança, a indimplência da população está entre {np.round(ic_inferior, 2)}% e {np.round(ic_superior, 2)}%.")
+              st.write(f"Com 90% de confiança, a proporção da população está entre {np.round(ic_inferior, 2)}% e {np.round(ic_superior, 2)}%.")
 
 
         
-        elif (tamanho_amostra_convertido * inad_positivo >= 10) and (tamanho_amostra_convertido * (1 - inad_positivo) < 10):
+        elif (tamanho_amostra * proporcao >= 5) and (tamanho_amostra * (1 - proporcao) < 5):
             st.markdown("<h1 style='font-size: 16px; color: gray; font-weight: bold;'>Premissas de volume:</h1>", unsafe_allow_html=True)
-            st.write("O número de inadimplentes é suficiente para o cálculo, mas o número de adimplentes não é.")
+            st.write("PROBLEMA: O número de sucesso é suficiente para o cálculo, mas o número de fracasso não é. Ambos precisam ser.")
             
-        elif (tamanho_amostra_convertido * inad_positivo < 10) and (tamanho_amostra_convertido * (1 - inad_positivo) >= 10):
+        elif (tamanho_amostra * proporcao < 5) and (tamanho_amostra * (1 - proporcao) >= 5):
             st.markdown("<h1 style='font-size: 16px; color: gray; font-weight: bold;'>Premissas de volume:</h1>", unsafe_allow_html=True)
-            st.write("O número de adimplentes é suficiente para o cálculo, mas o número de inadimplentes não é.")
+            st.write("PROBLEMA: O número de fracasso é suficiente para o cálculo, mas o número de sucesso não é. Ambos precisam ser.")
             
-        elif (tamanho_amostra_convertido * inad_positivo < 10) and (tamanho_amostra_convertido * (1 - inad_positivo) < 10):
+        elif (tamanho_amostra * proporcao < 5) and (tamanho_amostra * (1 - proporcao) < 5):
             st.markdown("<h1 style='font-size: 16px; color: gray; font-weight: bold;'>Premissas de volume:</h1>", unsafe_allow_html=True)
-            st.write("Os números de inadimplentes e adimplentes NÃO são suficientes para o cálculo.")
+            st.write("PROBLEMA: Os números de sucesso e fracasso NÃO são suficientes para o cálculo. Ambos precisam ser.")
